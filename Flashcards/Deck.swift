ceef62297eaf378a690fa10ce2b40ba5456a1a72
Yong Bakos <ybakos@humanoriented.com>
2015-06-23 20:44:43 -0600

    Lesson 3. Add Deck model with succinct use of map.

---
+++ Flashcards/Deck.swift
@@ -0,0 +1,22 @@
+/*
+
+This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike
+4.0 International License, by Yong Bakos.
+
+*/
+
+import Foundation
+
+class Deck {
+    
+    private let cards: [Flashcard]
+    
+    init() {
+        let cardData = [
+            "controller outlet" : "A controller view property, marked with IBOutlet",
+            "controller action" : "A controller method, marked with IBAction, that is triggered by an interface event."
+        ]
+        cards = map(cardData) { Flashcard(term: $0, definition: $1) }
+    }
+    
+}
